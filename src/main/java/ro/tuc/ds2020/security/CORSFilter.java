366dab508c29d53e0a558a8408e0ac4fdb280961
MihaiFilipDud <mihaifilipdud@gmail.com>
2021-01-14 08:51:39 +0200

    moddified cors policy

--- src/main/java/ro/tuc/ds2020/security/CORSFilter.java
+++ src/main/java/ro/tuc/ds2020/security/CORSFilter.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Configuration
 public class CORSFilter implements Filter {
 
-    private final List<String> allowedOrigins = Arrays.asList("http://localhost:3000", "https://mihaifilipdud-frontend-sd.herokuapp.com/");
+    private final List<String> allowedOrigins = Arrays.asList("http://localhost:3000", "https://mihaifilipdud-frontend-sd.herokuapp.com");
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         // Lets make sure that we are working with HTTP (that is, against HttpServletRequest and HttpServletResponse objects)
         if (req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
